e49e5108ea1b3b8324548f6fd555b35b4ed8b467
paritosh.c.at@gmail.com <P@ri_5862>
2020-11-10 14:58:53 +0000

    Working on urls.

--- InvoiceApp/urls.py
+++ InvoiceApp/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import users
+from .views import (
+    home_screen_view
+)
+from users.views import (
+    register_view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home_screen_view, name="home"),
+    path("register/", include(users.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
